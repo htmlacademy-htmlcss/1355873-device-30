aa2a0aed0b60a1257b24de706068091b00801c6a
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-02-25 22:09:50 +0300

    6.13. Сетка на флексах для внутренней страницы

--- js/script.js
+++ js/script.js
@@ -2,34 +2,43 @@
 
 let navigationButton = document.querySelector('.navigation-list_button');
 
-navigationButton.addEventListener('click', function () {
+navigationButton.addEventListener('click', function (evn) {
+    evn.preventDefault();
     navigationButton.classList.toggle('navigation-list_button--active');
 });
 
 // промо слайдер
 
-
-
-
 // попапы карта + обратная связь
     
 let wrapperPopupFeedback = document.querySelector('.wrapper-popup-feedback');
-let feedbackPopupToggle = document.getElementById('feedback-button');
+let feedbackPopupButton = document.getElementById('feedback-button');
 let buttonCloseFeedback = document.querySelector('.button-close-feedback');
+let formName = wrapperPopupFeedback.querySelector('#form-name');
+let formEmail = wrapperPopupFeedback.querySelector('#form-email');
+let formTextArea = wrapperPopupFeedback.querySelector('#form-textarea');
 
-feedbackPopupToggle.onclick = function () {
+
+
+feedbackPopupButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
     wrapperPopupFeedback.classList.add('popup-wrapper--active');
-};
+    formName.focus();
+})
 
-buttonCloseFeedback.onclick = function () {
+buttonCloseFeedback.addEventListener('click', function (evn) {
+    evn.preventDefault();
     wrapperPopupFeedback.classList.remove('popup-wrapper--active');
-}
+});
 
-window.onclick = function (e) {
-    if (e.target == wrapperPopupFeedback) {
-        wrapperPopupFeedback.classList.remove('popup-wrapper--active');
+window.addEventListener ('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+        if (wrapperPopupFeedback.classList.contains ('popup-wrapper--active')) {
+            evt.preventDefault();
+            wrapperPopupFeedback.classList.remove('popup-wrapper--active');
+        }
     }
-}
+});
 
 let wrapperPopupMap = document.querySelector('.wrapper-popup-map');
 let mapPopupToggle = document.getElementById('map-popup-link');
@@ -43,8 +52,18 @@ buttonCloseMap.onclick = function () {
     wrapperPopupMap.classList.remove('popup-wrapper--active');
 }
 
-window.onclick = function (e) {
-    if (e.target == wrapperPopupMap) {
+window.onclick = function (evt) {
+    if (evt.target == wrapperPopupMap) {
         wrapperPopupMap.classList.remove('popup-wrapper--active');
     }
 }
+
+
+window.addEventListener ('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+        if (wrapperPopupMap.classList.contains ('popup-wrapper--active')) {
+            evt.preventDefault();
+            wrapperPopupMap.classList.remove('popup-wrapper--active');
+        }
+    }
+});
